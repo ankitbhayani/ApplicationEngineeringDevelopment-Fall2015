ccd98bf812f7599c6091d7c4805159827f93c2fc
ankitbhayani <bhayani.ahusky.neu.edu>
2015-09-16 16:57:38 -0400

    Assignment-1 is in process

---
+++ AEDFALL2015Assignment1/src/Business/Person.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Swati
+ */
+public class Person {
+    
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String dateOfBirth;
+    private String streetAdress;
+    private String town;
+    private String zipCode;
+    private String occupation;
+    private String emailAddress;
+    private String areaCodeForPhone;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getStreetAdress() {
+        return streetAdress;
+    }
+
+    public void setStreetAdress(String streetAdress) {
+        this.streetAdress = streetAdress;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAreaCodeForPhone() {
+        return areaCodeForPhone;
+    }
+
+    public void setAreaCodeForPhone(String areaCodeForPhone) {
+        this.areaCodeForPhone = areaCodeForPhone;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
