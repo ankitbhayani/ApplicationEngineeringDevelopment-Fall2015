e83545d847c2716caae8cbc41c80513a0a77db07
ankitbhayani <bhayani.ahusky.neu.edu>
2015-09-23 16:48:56 -0400

    Assignment#2 is in progress

---
+++ Assignment#2/src/Business/Patient.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+public class Patient {
+    
+   private String patientName;
+   private String patientId;
+   private int age;
+   private String primaryDocName;
+   private String preferredPharmacy;
+   private ArrayList<VitalSign> vitalSignHistory;
+   
+   
+    public Patient(){
+        vitalSignHistory = new ArrayList<>();
+    }    
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPrimaryDocName() {
+        return primaryDocName;
+    }
+
+    public void setPrimaryDocName(String primaryDocName) {
+        this.primaryDocName = primaryDocName;
+    }
+
+    public String getPreferredPharmacy() {
+        return preferredPharmacy;
+    }
+
+    public void setPreferredPharmacy(String preferredPharmacy) {
+        this.preferredPharmacy = preferredPharmacy;
+    }
+
+    public ArrayList<VitalSign> getVitalSignHistory() {
+        return vitalSignHistory;
+    }
+
+    public void setVitalSignHistory(ArrayList<VitalSign> vitalSignHistory) {
+        this.vitalSignHistory = vitalSignHistory;
+    }
+   
+       public VitalSign addVitalSign(){
+        VitalSign vitalSign= new VitalSign();
+        vitalSignHistory.add(vitalSign);
+        return vitalSign;
+    }
+    
+    public void removeVitalSign(VitalSign vitalSign){
+        vitalSignHistory.remove(vitalSign);
+    }
+     
+
+}
---
+++ Assignment#2/src/Business/VitalSign.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+
+public class VitalSign {
+    
+    private float respiratoryRate;
+    private float heartRate;
+    private float systolicBloodPressure;
+    private float Weight;
+    private String timestamp;
+    private String state;
+
+    public float getRespiratoryRate() {
+        return respiratoryRate;
+    }
+
+    public void setRespiratoryRate(float respiratoryRate) {
+        this.respiratoryRate = respiratoryRate;
+    }
+
+    public float getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(float heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public float getSystolicBloodPressure() {
+        return systolicBloodPressure;
+    }
+
+    public void setSystolicBloodPressure(float systolicBloodPressure) {
+        this.systolicBloodPressure = systolicBloodPressure;
+    }
+
+    public float getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(float Weight) {
+        this.Weight = Weight;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString(){
+        return timestamp;
+    }
+
+           
+}
